c83cfc91375bfa48f6761d84f65bccd9165a3d1f
Stanislav <stanislav.dyachenko@feedvisor.com>
2026-07-30 21:51:11 +0300

    MainPage structure

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -1,7 +1,30 @@
+import { expect, Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class MainPage extends BasePage {
+  private readonly haederLocator: Locator;
+  private readonly categoriesTabsLocator: Locator;
+  private readonly menuLocator: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.haederLocator = this.page.getByRole('banner');
+    // this.ctegoriesTabsLocator = this.page.locator('section').filter({ hasText: /ГлавнаяЧМ-2026/ });
+    this.categoriesTabsLocator = this.page.getByText('ГлавнаяЧМ-2026');
+    this.menuLocator = this.page.getByLabel('Облегченная панель навигации');
+  }
+
   async open() {
-    this.page.goto('https://rutube.ru/');
+    await this.page.goto('https://rutube.ru/');
+  }
+
+  async headerHasAriaSnapshot() {
+    await expect(this.haederLocator).toMatchAriaSnapshot();
+  }
+  async categoriesHasAriaSnapshot() {
+    await expect(this.categoriesTabsLocator).toMatchAriaSnapshot();
+  }
+  async menuHasAriaSnapshot() {
+    await expect(this.menuLocator).toMatchAriaSnapshot();
   }
 }
--- tests/specs/unauthorized/mainPage.spec.ts
+++ tests/specs/unauthorized/mainPage.spec.ts
@@ -2,15 +2,18 @@ import { test, expect } from '@playwright/test';
 import { MainPage } from '../../pages/MainPage';
 
 test.beforeEach(async ({ page }) => {
-  // Точный составной селектор на основе анализа тегов
-  const popupCloseButton = page.locator(
-    'div[data-testid="popup"] button[aria-label="Закрыть попап"]',
-  );
+  const popupCloseButton = page.getByTestId('popup').getByRole('button', { name: 'Закрыть попап' });
+  const cookieCloseButton = page.getByRole('button', { name: 'Ок', exact: true });
 
   // Регистрируем фоновый обработчик прерываний (Interceptors)
   await page.addLocatorHandler(popupCloseButton, async (locator) => {
     await locator.click();
     console.log('--- [Playwright Auto-Close] Попап рекламы успешно закрыт ---');
+  });
+
+  await page.addLocatorHandler(cookieCloseButton, async (locator) => {
+    await locator.click();
+    console.log('--- [Playwright Auto-Close] Попап куков успешно закрыт ---');
   });
 });
 
@@ -20,5 +23,30 @@ test('> Open main page', async ({ page }) => {
   await mainPage.open();
   await expect(page).toHaveTitle(/rutube/i);
 
-  //   await page.waitForLoadState('networkidle');
+  await page.waitForLoadState();
+});
+
+test('> Check header elements avaliable', async ({ page }) => {
+  const mainPage = new MainPage(page);
+
+  await mainPage.open();
+  // await page.getByRole('button', { name: 'Ок', exact: true }).click();
+
+  await mainPage.headerHasAriaSnapshot();
+});
+
+test('> Check categories tab elements avaliable', async ({ page }) => {
+  const mainPage = new MainPage(page);
+
+  await mainPage.open();
+
+  await mainPage.categoriesHasAriaSnapshot();
+});
+
+test('> Check menu tab elements avaliable', async ({ page }) => {
+  const mainPage = new MainPage(page);
+
+  await mainPage.open();
+
+  await mainPage.menuHasAriaSnapshot();
 });
